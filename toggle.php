cc83649c6c4a069d423f244b6936582d4a3a119d
Marc <sanmarcos@users.noreply.github.com>
2016-02-15 09:28:18 -0800

    Separate configuration into separate file so it is easier to pull new source code without overriding your outlet codes

--- toggle.php
+++ toggle.php
@@ -2,40 +2,8 @@
 header('Content-Type: application/json');
 header('Cache-Control: no-cache, must-revalidate');
 
-// Edit these codes for each outlet
-$codes = array(
-    "1" => array(
-        "on" => 349491,
-        "off" => 349500
-    ),
-    "2" => array(
-        "on" => 349635,
-        "off" => 349644
-    ),
-    "3" => array(
-        "on" => 349955,
-        "off" => 349964
-    ),
-    "4" => array(
-        "on" => 351491,
-        "off" => 351500
-    ),
-    "5" => array(
-        "on" => 357635,
-        "off" => 357644
-    ),
-);
-
-// Path to the codesend binary (current directory is the default)
-$codeSendPath = './codesend';
-
-// This PIN is not the first PIN on the Raspberry Pi GPIO header!
-// Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
-// for more information.
-$codeSendPIN = "0";
-
-// Pulse length depends on the RF outlets you are using. Use RFSniffer to see what pulse length your device uses.
-$codeSendPulseLength = "189";
+// Please edit this file with your outlet codes and pulse length
+require_once('configuration.inc.php');
 
 if (!file_exists($codeSendPath)) {
     error_log("$codeSendPath is missing, please edit the script", 0);
@@ -44,6 +12,11 @@ if (!file_exists($codeSendPath)) {
 
 $outletLight = $_POST['outletId'];
 $outletStatus = $_POST['outletStatus'];
+
+if (is_empty($outletLight) || is_empty($outletStatus)) {
+    error_log('Missing POST paremeters', 0);
+    die(json_encode(array('success' => false)));
+}
 
 if ($outletLight == "6") {
     // 6 is all 5 outlets combined
